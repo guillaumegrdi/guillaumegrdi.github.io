6647d41bbf093c4582697876f702751be3799fab
guillaume.grdi <guillaumegrandi2008@gmail.com>
2024-05-29 17:33:59 +0200

    update responsove

--- python.js
+++ python.js
@@ -3,88 +3,13 @@ const lineContainer = document.querySelector(".lineContainer");
 
 terminalArea.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
-    if (e.target.value == 'print("hello world")') {
-      e.target.value = `print("hello world")
-hello world`;
-      lineContainer.innerHTML += `
-      <div class="line">
-          <span>>>></span>
-      </div> `;
+    if (e.target.value == "download()" || e.target.value == "download") {
+      const promptValue = prompt("Quel fichier souhaitez vous télécharger ?");
+      window.open("python/" + promptValue);
     }
-
-    if (e.target.value == 'download("triangle.py")') {
-      window.open("./python/Triangle.py");
-    }
-
-    if (e.target.value == 'launch("triangle.py")') {
-      function triangle() {
-        let l1 = parseInt(
-          prompt("veuillez entrer la longueur du premier coté :  ")
-        );
-        let l2 = parseInt(
-          prompt("veuillez entrer la longueur du second coté  :  ")
-        );
-        let l3 = parseInt(
-          prompt("veuillez entrer la longueur du dernier coté :  ")
-        );
-
-        function checkValue() {
-          if (l1 == 0) {
-            alert("veuillez entrer des valeurs correctes");
-            triangle();
-          }
-          if (l2 == 0) {
-            alert("veuillez entrer des valeurs correctes");
-            triangle();
-          }
-          if (l3 == 0) {
-            alert("veuillez entrer des valeurs correctes");
-            triangle();
-          }
-        }
-        checkValue();
-
-        function checkTriangle() {
-          if (l1 ** 2 + l2 ** 2 == l3 ** 2) {
-            e.target.value = `launch("triangle.py")
-le triangle est rectangle`;
-          } else if (l2 ** 2 + l3 ** 2 == l1 ** 2) {
-            e.target.value = `launch("triangle.py")
-le triangle est rectangle`;
-          } else if (l1 ** 2 + l3 ** 2 == l2 ** 2) {
-            e.target.value = `launch("triangle.py")
-le triangle est rectangle`;
-          } else if (l1 + l2 == l3) {
-            e.target.value = `launch("triangle.py")
-le triangle est impossible`;
-          } else if (l1 + l3 == l2) {
-            e.target.value = `launch("triangle.py")
-le triangle est impossible`;
-          } else if (l3 + l2 == l1) {
-            e.target.value = `launch("triangle.py")
-le triangle est impossible`;
-          } else {
-            e.target.value = `launch("triangle.py")
-le triangle n"est pas rectangle`;
-          }
-        }
-        checkTriangle();
-      }
-
-      triangle();
-      lineContainer.innerHTML += `
-      <div class="line">
-          <span>>>></span>
-      </div> `;
-    }
-
-    // ---------------
     lineContainer.innerHTML += `
       <div class="line">
           <span>>>></span>
       </div> `;
-  }
-  if (e.key == "Backspace") {
-    lineContainer.innerHTML = lineContainer.innerHTML;
   }
 });
--- python/Triangle.py
+++ python/Triangle.py
@@ -1,27 +1,21 @@
-def triangle() :
+def triangle():
 
-    l1 = int(input('veuillez entrer la longueur du premier coté :  ' ))
-    l2 = int(input('veuillez entrer la longueur du second coté :   '))
-    l3 = int(input('veuillez entrer la longueur du dernier coté :   '))
-    
-    # if l1 or l2 or l3 == 0:
-    #   print('veuillez entrer des valeurs correctes')
-    #   triangle()
+  l1 = int(input('veuillez entrer la longueur du premier coté :  '))
+  l2 = int(input('veuillez entrer la longueur du second coté :   '))
+  l3 = int(input('veuillez entrer la longueur du dernier coté :   '))
 
-    if l1**2 + l2**2 == l3**2:
-     print('le triangle est rectangle')
-    elif l2**2 + l3**2 == l1**2:
-     print('le triangle est rectangle')
-    elif l1**2 + l3**2 == l2**2:
-     print('le triangle est rectangle')
-    elif l1 + l2 == l3:
-     print('le triange est impossible')
-    else:
-     print('le triangle n est pas rectangle')
+  if l1**2 + l2**2 == l3**2 or l2**2 + l3**2 == l1**2 or l1**2 + l3**2 == l2**2:
+    print('le triangle est rectangle')
+  elif l1 + l2 <= l3 or l3 + l2 <= l1 or l1 + l3 <= l2:
+    print('le triange est impossible')
+  else:
+    print('le triangle n est pas rectangle')
 
-    replay = int(input("voulez vous refaire ?   "))
+  replay = int(input("voulez vous refaire ?  "))
 
-    if replay == 1:
-      triangle()
+  if replay == 1:
+    triangle()
+
 
 triangle()
+
